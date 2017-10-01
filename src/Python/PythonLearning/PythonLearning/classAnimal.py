bad5ab0d529b69c9d5e96d231d84da7c48220cd9
shuguanl <honkliu@hotmail.com>
2017-10-01 13:02:53 +0800

    class

---
+++ src/Python/PythonLearning/PythonLearning/classAnimal.py
@@ -0,0 +1,23 @@
+class classAnimal(object):
+    """description of class"""
+    def __init__(self, weight) :
+        self.weight = weight
+
+    def __add__(self, other) :
+        return classAnimal(self.weight + other.weight)
+
+    def __str__(self):
+        return 'Animal (%d)' %self.weight
+
+    def Name(self) :
+        print("Hello")
+
+dog = classAnimal(15)
+dog.Name()
+
+cat = classAnimal(10)
+cat.Name()
+
+print(dog+cat)
+    
+
